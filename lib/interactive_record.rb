09eab4d8dfc26721a3f6d38197a989b27da914fd
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-31 22:02:14 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,6 +57,13 @@ class InteractiveRecord
   end
 
   def self.find_by(find_by)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end 
+
+
+
+
   end
 
 
